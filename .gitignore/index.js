685613a64e6c742e1fe2010555db75f1d3246834
Satoru3 <38013927+Satoru3@users.noreply.github.com>
2018-11-24 23:22:54 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -57,7 +57,7 @@ bot.on('message', message => {
             .addField(":crossed_swords: Compétence 3: Sword of the Heir ", "Attaque tous les ennemis avec une tempête d'épée et récupère de la santée proportionnellement aux dégâts infligés. Les dégâts infligés augmentent proportionnellement à la santée maximale du lanceur.", false)
             .addField(":comet: Vidéo d'introduction du personnage", ":arrow_down:", true)
 	message.channel.sendEmbed(embed);
-        message.react('❤️');
+        message.react('❤');
 	console.log("La commande du Héros Ras viens d'être effectuée.");
 
     }
