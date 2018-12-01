b25c573a8acc30dc6e459c89f4f84a5815a9cc44
Satoru3 <38013927+Satoru3@users.noreply.github.com>
2018-12-01 17:00:13 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -33,7 +33,7 @@ bot.on('message', message => {
     }
 
     if(message.content.startsWith(prefix + "purge")){
-        if(!message.guild.member(message.author).hasPermission("MANAGE_MESSAGE")) return message.channel.send("Vous n'avez pas la permission d'utiliser cette commande !");
+        if(!message.guild.member(message.author).hasPermission("MANAGE_MESSAGES")) return message.channel.send("Vous n'avez pas la permission d'utiliser cette commande !");
             
         let args = message.content.split(" ").slice(1);
 
