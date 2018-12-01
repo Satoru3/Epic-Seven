d8b8d1c6b6d95a87d1619a982639cefd19aeceec
Satoru3 <38013927+Satoru3@users.noreply.github.com>
2018-12-01 17:56:13 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -26,36 +26,6 @@ bot.on('message', message => {
         console.log("Page d'aide générée suite à la demande de " + message.author.username);
 	
     }
-	
-    if (message.content === prefix + "purge"){
-	const user = message.mentions.users.first();
-	if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('Sorry, you don\'t have permission to delete or purge messages!')
-		.then(msg => msg.delete({
-			timeout: 10000
-		}));
-	const amount = !!parseInt(message.content.split(' ')[1]) ? parseInt(message.content.split(' ')[1]) : parseInt(message.content.split(' ')[2])
-	if (!amount) return message.channel.send('Specify an amount of messages to delete or purge!')
-		.then(msg => msg.delete({
-			timeout: 10000
-		}));
-	if (!amount && !user) return message.channel.send('Specify a user and amount, or just an amount, of messages to delete or purge!')
-		.then(msg => msg.delete({
-			timeout: 10000
-		}));
-	message.channel.messages.fetch({
-			limit: amount
-		, })
-		.then((messages) => {
-			if (user) {
-				const filterBy = user ? user.id : client.user.id;
-				messages = messages.filter(m => m.author.id === filterBy)
-					.array()
-					.slice(0, amount);
-			}
-			message.channel.bulkDelete(messages)
-				.catch(error => console.log(error.stack));
-		});
-    }
 
     if (message.content === prefix +"e7"){
     message.channel.send("**Utilisation correcte** : $e7 <personnage> | Si tu ne connais pas les personnages disponible, utilise la commande $personnages.")
