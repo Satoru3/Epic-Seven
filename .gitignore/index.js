20780d49c9f7245a889885352ca0b296c0a84485
Satoru3 <38013927+Satoru3@users.noreply.github.com>
2019-03-06 22:22:29 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -364,6 +364,25 @@ bot.on('message', message => {
    
     }
 
+  if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("Sorry pal, you can't do that.");
+  let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+  if(!rMember) return message.reply("Couldn't find that user, yo.");
+  let role = args.join(" ").slice(22);
+  if(!role) return message.reply("Specify a role!");
+  let gRole = message.guild.roles.find(`name`, role);
+  if(!gRole) return message.reply("Couldn't find that role.");
+
+  if(rMember.roles.has(gRole.id)) return message.reply("They already have that role.");
+  await(rMember.addRole(gRole.id));
+
+  try{
+    await rMember.send(`Congrats, you have been given the role ${gRole.name}`)
+  }catch(e){
+    console.log(e.stack);
+    message.channel.send(`Congrats to <@${rMember.id}>, they have been given the role ${gRole.name}. We tried to DM them, but their DMs are locked.`)
+ 
+   }
+
     if (message.content === prefix + "botinfos"){ //Commande d'infos sur le bot
         let boticon = bot.user.displayAvatarURL;
         let botembed = new Discord.RichEmbed()
