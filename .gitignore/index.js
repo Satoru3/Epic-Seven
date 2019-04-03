ed7658d9de1a3db3e8aed1ebca2bd5308fb01911
Satoru3 <38013927+Satoru3@users.noreply.github.com>
2019-04-03 16:25:24 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -433,3 +433,21 @@ bot.on("message", function(message) {
         console.log("La commande 8ball viens d'être effectuée avec succès par " + message.author.username);
 
     }})
+
+
+bot.on('message', msg => {
+    if(msg.content[0] === prefix) {
+       if(msg.content === prefix + 'gros château') {
+	  let role = msg.guild.roles.find('name', 'gros château')
+	  
+	  if(msg.member.roles.find('name', 'gros château')) {
+	      msg.member.removeRole(role)
+	      msg.reply(`Rôle retiré Gros Château`)
+	  }
+	   else {
+	      msg.member.addRole(role)
+	      msg.reply(`Ajout du rôle Gros Château`)
+	   }
+       }
+    }
+})
