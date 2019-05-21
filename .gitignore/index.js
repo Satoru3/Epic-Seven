4f66f5e8e906a199b05965180a3eddaab0dd97c4
Satoru3 <38013927+Satoru3@users.noreply.github.com>
2019-05-21 16:53:23 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -368,41 +368,6 @@ bot.on('message', message => {
 	message.channel.sendEmbed(embed)
 	    
     }
-	
-	    if (message.content === prefix + "userinfo") {
-
-        const UserInfo = new Discord.MessageEmbed()
-
-            .setAuthor(message.author.username, message.author.avatarURL()) //Heading With Username & Their Avatar 
-            .setTitle('Infos utilisateur')
-            .setColor('RANDOM') //You Can Use HexColour Ex:- #000000
-            .setImage(message.author.avatarURL()) //Add Any Image URl || Image
-            .setThumbnail(message.author.avatarURL()) //Add Any Image URl || ThumbNail
-
-
-            .addField('Avatar', message.author.avatar, true) //The ID of the user's avatar //Inline True or false
-
-            .addField('Bot', message.author.bot, true) //Returns True If Message Author = Bot || False If Message Author not Bot.
-            .addField('Créé le', message.author.createdAt, false) //The time the user was created || .createdTimestamp - The timestamp the user was created at
-            .addField('Discrimateur', message.author.discriminator, true) //A discriminator/tag based on username for the user Ex:- 0001
-            .addField('DMChannel', message.author.dmChannel) //The DM between the client's user and this user || If Nothing Returns "Null"
-            .addField('ID', message.author.id) //The ID of the User/author
-            .addField('Dernier message', message.author.lastMessage) //The Message object of the last message sent by the user, if one was sent
-            .addField('ID du dernier message', message.author.lastMessageID) //The ID of the last message sent by the user, if one was sent
-            .addField('Présente ?', message.author.presence) //The presence of this user
-            .addField('Présent', message.author.presence.status) //The presence status of this user
-            .addField('Joue à', message.author.presence.activity.name) //The presence Game of this user
-            .addField('Tag', message.author.tag) //The Discord "tag" for this user || Ex:- Sai Chinna#6718
-            .addField('Pseudo', message.author.username) //The username of the user || Ex:- Sai Chinna
-            .addField('Nick Name', message.guild.member(target).displayName) //Nick Name In That (message sent) server || Define target as message Author Ex:- let target = message.author; || Add This Line in Top
-
-            .setFooter('Demandé par ', message.author.tag) //Change To Anything As You Wish
-            .setTimestamp() //The timestamp of this embed
-
-        message.channel.send(UserInfo);
- 
-	    }
-	
 
 	
 	// Commande d'avatar
