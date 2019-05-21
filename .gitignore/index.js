23e23e35c86a10944c5c15a6ce4787ef46b95d73
Satoru3 <38013927+Satoru3@users.noreply.github.com>
2019-05-21 15:15:49 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -370,6 +370,14 @@ bot.on('message', message => {
     }
 	
 	
+    if (message.channel.id === '508389383188250624') {
+        if (isNaN(message.content)) {
+            message.delete()
+            message.channel.send('Seules les images sont autorisées dans ce salon.')
+		
+    }
+	    
+	
 	// Commande d'avatar
     if (message.content.startsWith (prefix + "avatar")){
        let user = message.mentions.users.first() || message.author;
