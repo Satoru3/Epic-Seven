7aad9ce4ab892cb683c5c3738850bc45ecec1f6f
Satoru3 <38013927+Satoru3@users.noreply.github.com>
2019-05-21 16:37:37 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -389,17 +389,17 @@ bot.on('message', message => {
                 size: '2048'
             }), true)
             .addField('Bot', message.author.bot, true) //Returns True If Message Author = Bot || False If Message Author not Bot.
-            .addField('Created At', message.author.createdAt, false) //The time the user was created || .createdTimestamp - The timestamp the user was created at
-            .addField('Discrim', message.author.discriminator, true) //A discriminator/tag based on username for the user Ex:- 0001
+            .addField('Créé le', message.author.createdAt, false) //The time the user was created || .createdTimestamp - The timestamp the user was created at
+            .addField('Discrimateur', message.author.discriminator, true) //A discriminator/tag based on username for the user Ex:- 0001
             .addField('DMChannel', message.author.dmChannel) //The DM between the client's user and this user || If Nothing Returns "Null"
             .addField('ID', message.author.id) //The ID of the User/author
-            .addField('Last Message', message.author.lastMessage) //The Message object of the last message sent by the user, if one was sent
-            .addField('Last Message ID', message.author.lastMessageID) //The ID of the last message sent by the user, if one was sent
-            .addField('Presence', message.author.presence) //The presence of this user
-            .addField('Presence Status', message.author.presence.status) //The presence status of this user
-            .addField('Presence Game', message.author.presence.activity.name) //The presence Game of this user
+            .addField('Dernier message', message.author.lastMessage) //The Message object of the last message sent by the user, if one was sent
+            .addField('ID du dernier message', message.author.lastMessageID) //The ID of the last message sent by the user, if one was sent
+            .addField('Présente ?', message.author.presence) //The presence of this user
+            .addField('Présent', message.author.presence.status) //The presence status of this user
+            .addField('Joue à', message.author.presence.activity.name) //The presence Game of this user
             .addField('Tag', message.author.tag) //The Discord "tag" for this user || Ex:- Sai Chinna#6718
-            .addField('Username', message.author.username) //The username of the user || Ex:- Sai Chinna
+            .addField('Pseudo', message.author.username) //The username of the user || Ex:- Sai Chinna
             .addField('Nick Name', message.guild.member(target).displayName) //Nick Name In That (message sent) server || Define target as message Author Ex:- let target = message.author; || Add This Line in Top
 
             .setFooter('Demandé par ', message.author.tag) //Change To Anything As You Wish
