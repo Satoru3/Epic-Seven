50742a6c9a2575a763835c02eaa434b55eaf1448
Satoru3 <38013927+Satoru3@users.noreply.github.com>
2019-05-28 20:42:54 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -6,6 +6,7 @@ var prefix = ("$")
 bot.on('ready', function() {
     bot.user.setActivity("$help | V 1.2.2 | Ajout de 3 artefacts. ");
     console.log("Je suis connecté !");
+
 });
 
 bot.login(process.env.TOKEN);
@@ -567,23 +568,6 @@ bot.on("message", async message => {
   const command = args.shift().toLowerCase();
   
   // Let's go with a few common example commands! Feel free to delete or change those.
-  
-  if(command === "ping") {
-    // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
-    // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
-    const m = await message.channel.send("Ping?");
-    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-  }
-  
-  if(command === "say") {
-    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
-    // To get the "message" itself we join the `args` back into a string with spaces: 
-    const sayMessage = args.join(" ");
-    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
-    message.delete().catch(O_o=>{}); 
-    // And we get the bot to say the thing: 
-    message.channel.send(sayMessage);
-  }
   
   if(command === "kick") {
     // This command must be limited to mods and admins. In this example we just hardcode the role names.
