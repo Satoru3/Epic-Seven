240c0feeba0d9aa6002e124392fcb14797597811
Satoru3 <38013927+Satoru3@users.noreply.github.com>
2019-08-30 20:31:22 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -499,7 +499,7 @@ bot.on("message", function(message) {
 
             let reponse = (replys[Math.floor(Math.random() * replys.length)])
             var bembed = new Discord.RichEmbed()
-            .setDescription(" :<a:aPeepoCheer:422063843938926602>: 26ball :<a:aPeepoCheer:422063843938926602>:")
+            .setDescription(" <a:aPeepoCheer:422063843938926602> 26ball <a:aPeepoCheer:422063843938926602>")
             .addField(message.author.username + " :", tte)
             .addField("Epic Bot :", reponse)
             .setThumbnail(message.author.avatarURL)
