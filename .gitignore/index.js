f9a99ab6c3b9021bd3776b27b94c16c26d368b19
Satoru3 <38013927+Satoru3@users.noreply.github.com>
2020-05-18 19:07:35 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -68,7 +68,7 @@ bot.on('message', message => {
         if (message.content === prefix + "sorties"){ //Commande relative aux artefacts d'Epic Seven
         var embed = new Discord.RichEmbed()
             .setTitle(":black_small_square: **Sorties** :black_small_square:")
-            .addField("<:UriahMilk:711978620242886736> **Uriah**", true)
+            .addField("<:UriahMilk:711978620242886736> **Uriah**")
             .addField("**Born from Death**","Durandal 5 <:etoile:580331911608664084> <:e7combattant:506111308635308032>", true)
             .addField("**OOPARTS**","Recevoir les notifications d'OOPARTS <:Ooparts:711972771214655660>", true)
             .addField("**$border**","Border Coin 5 <:etoile:580331911608664084> <:e7combattant:506111308635308032>", true)
